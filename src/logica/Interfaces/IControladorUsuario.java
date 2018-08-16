8c261e5def6281c4cac6750a897edb4ae110561c
Martin <Martin@DESKTOP-1TL7PQ1>
2018-08-16 17:27:03 -0300

    putos

--- src/logica/Interfaces/IControladorUsuario.java
+++ src/logica/Interfaces/IControladorUsuario.java
@@ -7,6 +7,7 @@ package logica.Interfaces;
 
 import java.util.Calendar;
 import java.util.Date;
+import logica.Clases.Proponente;
 
 /**
  *
@@ -15,11 +16,9 @@ import java.util.Date;
 public interface IControladorUsuario {
 
     public abstract void seguirUsuario(String nickUsu1, String nickUsu2);
-
+    public abstract void copiarFoto(String foto, String nick);
     public abstract void dejarseguirUsuario(String nickUsu1, String nickUsu2);
-
     public abstract boolean AgregarUsuarioColaborador(String nickName, String nombre, String apellido,String correo, Calendar fechaN, String imagen);
-
     public abstract boolean AgregarUsuarioProponente(String nickName, String nombre, String apellido,String correo, Calendar fechaN, String imagen, String direccion, String biografia, String sitioWeb);
-
+    public abstract Proponente ObtenerProponente(String nombreP);
 }
