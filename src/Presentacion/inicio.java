31159eac41aaa0c391836986be655b02e80615ee
Lucas <Lucas@DESKTOP-CUER15B>
2018-09-04 19:11:35 -0300

    detalles

--- src/Presentacion/inicio.java
+++ src/Presentacion/inicio.java
@@ -292,7 +292,6 @@ public class inicio extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem8ActionPerformed
 
     private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
-
         Fabrica fabrica = Fabrica.getInstance();
 
         int respuesta = JOptionPane.showConfirmDialog(null, "Se borraran todos los datos de la base de datos\n Desea continuar?", "ADVERTENCIA", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
